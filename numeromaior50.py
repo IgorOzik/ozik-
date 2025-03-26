93168f73523eff8602324f872494b1684607ad37
IgorOzik <igorozikvasconcelosfilho@gmail.com>
2025-03-26 20:51:14 -0300

    Add files via upload

---
+++ numeromaior50.py
@@ -0,0 +1,7 @@
+numero = float(input('Digite um Número: '))
+if numero > 50:
+    print(f'Seu número {numero} é maior que 50')
+elif numero == 50:
+    print(f'Seu número ({numero}) é 50')
+else:
+    print(f'seu número {numero} é menor que 50')
